00b5ddec92e6c2eb639021cde14ebcd2fb2ff35c
twocliffsgames <twocliffsgames@gmail.com>
2017-06-05 00:36:44 +0300

    Version 0.0.1 lmao.

---
+++ Truth or DareUITests/Truth_or_DareUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Truth_or_DareUITests.swift
+//  Truth or DareUITests
+//
+//  Created by Александр Колосов on 04.06.17.
+//  Copyright © 2017 Two Cliffs Games. All rights reserved.
+//
+
+import XCTest
+
+class Truth_or_DareUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
